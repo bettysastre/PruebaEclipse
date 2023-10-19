55bf99f87a11920cf1fabc0f0409406c656a5a0b
Bettysastre <beatrizara1401@gmail.com>
2023-10-19 07:47:40 -0600

    se creó la clase ejemploGit

---
+++ ejemploGit.java
@@ -0,0 +1,12 @@
+package pruebaGit;
+
+public class ejemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola alumnos de git");
+		String nombre ="Beatriz Sastre";
+
+	}
+
+}
